f430a4123e2f2f59c84bab491f4d0a2d7419d96a
Itai shainer <itaishainer22@gmail.com>
2025-11-26 20:47:20 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,14 +1,12 @@
 import MeetPoint from './pages/MeetPoint';
 import RouteHistory from './pages/RouteHistory';
 import Favorites from './pages/Favorites';
-import utils from './pages/utils';
 
 
 export const PAGES = {
     "MeetPoint": MeetPoint,
     "RouteHistory": RouteHistory,
     "Favorites": Favorites,
-    "utils": utils,
 }
 
 export const pagesConfig = {
